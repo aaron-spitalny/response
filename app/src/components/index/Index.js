a9c42b9dcb54ac56c1c9b721178ddbebbc89b05d
aaron-spitalny <aarons3833@gmail.com>
2019-05-15 10:51:00 -0400

    bug fix

--- app/src/components/index/Index.js
+++ app/src/components/index/Index.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import Header from "./Header";
 import Body from "./Body";
-import jsonData from "../../../../../processed-cdaad.json";
 
 function Index(props) {
 	return (
